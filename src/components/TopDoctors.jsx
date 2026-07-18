60ac6694202e8d641ef43e836b1f39bfb115bfb5
Rakeshbhandary08 <rakeshbhandary08@gmail.com>
2026-07-18 17:15:49 +0530

    addition

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -6,7 +6,7 @@ const TopDoctors = () => {
     const navigate=useNavigate();
   return (
     <div className="  flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
-      <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
+      <h1 className="text-3xl font-medium">Top Doctors to Book:</h1>
       <p className="text-sm sm:w-1/3 text-center">
         Simply browse through our extensive list of trusted doctors.
       </p>
